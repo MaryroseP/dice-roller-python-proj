182e5139d9e6f06bf49da55bdf4d68c2cd8be1a6
Pergis_M <maryrosepergis@gmail.com>
2024-09-13 21:35:52 +0800

    Added a bit of code I tried to solve my own problem but I haven't gotten it yet. Will continue soon.

--- main.py
+++ main.py
@@ -42,3 +42,18 @@ dice_art = {
         "└─────────┘")
 }
 
+dice = []   # list
+total = 0
+num_of_dice = int(input("How many dice?: "))
+
+for die in range(num_of_dice):
+    dice.append(random.randint(1,6))
+
+for die in range(num_of_dice):
+    for line in dice_art.get(dice[die]):
+        print(f"{line}",end=" ") # tried to solve my own problem
+
+for die in dice:
+    total += die
+
+print(f"Total: {total}")
